1cd908f0d823d5e001367c0a3a6fdc7d754f3f3d
DANIEL SALAS ALONSO <dsa069@inlumine.ual.es>
2026-04-24 01:18:29 +0200

    Cielo para cada escenario

--- Assets/Scripts/DayAndNight.cs
+++ Assets/Scripts/DayAndNight.cs
@@ -8,9 +8,11 @@ public class DayAndNight : MonoBehaviour
    [Header("Materiales del Skybox")]
     public Material simpleSkyMaterial;
     public Material realStarsMaterial;
+    public Material sunsetSkyMaterial;
+    public Material superNovaSkyMaterial;
 
     // Función 1: Cambia al cielo simple
-    public void SetDay()
+    public void SetForestDay()
     {
         if (simpleSkyMaterial != null)
         {
@@ -21,13 +23,35 @@ public class DayAndNight : MonoBehaviour
     }
 
     // Función 2: Cambia al cielo de estrellas
-    public void SetNight()
+    public void SetDarkNight()
     {
         if (realStarsMaterial != null)
         {
             RenderSettings.skybox = realStarsMaterial;
             DynamicGI.UpdateEnvironment();
             Debug.Log("Cielo cambiado a: Real Stars");
+        }
+    }
+
+    // Función 3: Cambia el cielo a atardecer
+    public void SetBeachSunset()
+    {
+        if (sunsetSkyMaterial != null)
+        {
+            RenderSettings.skybox = sunsetSkyMaterial;
+            DynamicGI.UpdateEnvironment();
+            Debug.Log("Cielo cambiado a: Atardecer");
+        }
+    }
+
+    // Función 4: Cambia el cielo a una supernova
+    public void SetWhiteSuperNova()
+    {
+        if (superNovaSkyMaterial != null)
+        {
+            RenderSettings.skybox = superNovaSkyMaterial;
+            DynamicGI.UpdateEnvironment();
+            Debug.Log("Cielo cambiado a: Supernova");
         }
     }
 }
