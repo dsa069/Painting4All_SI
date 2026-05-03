254744a28d1c7da8cf1039af78c45db1f636bba4
Pablo Gómez Rivas <pgr866@inlumine.ual.es>
2026-05-03 16:49:14 +0200

    activar/desactivar mr

--- Assets/Scripts/PassthroughToggle.cs
+++ Assets/Scripts/PassthroughToggle.cs
@@ -1,22 +1,31 @@
 using UnityEngine;
+using Meta.XR.MRUtilityKit;
 
 public class PassthroughToggler : MonoBehaviour
 {
     [Header("Referencias Necesarias")]
     [SerializeField] private OVRPassthroughLayer passthroughLayer;
     [SerializeField] private Camera mainCamera;
+    [SerializeField] private GameObject locomotionSystem;
+
     public bool IsMRActive { get; private set; } = false;
+
+    private void Awake()
+    {
+        UnityEngine.XR.XRSettings.useOcclusionMesh = false;
+    }
+
     private void Start()
     {
         ToggleMR(false);
     }
+
     public void ToggleMR(bool enableMR)
     {
         IsMRActive = enableMR;
-        if (passthroughLayer != null)
-        {
-            passthroughLayer.enabled = enableMR;
-        }
+
+        if (passthroughLayer != null) passthroughLayer.enabled = enableMR;
+
         if (mainCamera != null)
         {
             if (enableMR)
@@ -29,6 +38,46 @@ public class PassthroughToggler : MonoBehaviour
                 mainCamera.clearFlags = CameraClearFlags.Skybox;
             }
         }
-        Debug.Log("Modo MR cambiado a: " + (enableMR ? "ON" : "OFF"));
+
+        if (locomotionSystem != null) locomotionSystem.SetActive(!enableMR);
+
+        if (MRUK.Instance != null)
+        {
+            MRUK.Instance.EnableWorldLock = enableMR;
+            if (enableMR && MRUK.Instance.GetCurrentRoom() == null)
+            {
+                MRUK.Instance.LoadSceneFromDevice();
+            }
+        }
+
+        ToggleAllEffectMeshes(enableMR);
+        Debug.Log("Modo MR: " + (enableMR ? "ON (Bloqueado & Locomotion OFF)" : "OFF (Libre & Locomotion ON)"));
+    }
+
+    private void ToggleAllEffectMeshes(bool visible)
+    {
+        EffectMesh[] allMeshes = Resources.FindObjectsOfTypeAll<EffectMesh>();
+        foreach (EffectMesh em in allMeshes)
+        {
+            if (em.gameObject.scene.name != null)
+            {
+                em.enabled = visible;
+                MeshRenderer[] renderers = em.GetComponentsInChildren<MeshRenderer>(true);
+                foreach (MeshRenderer mr in renderers)
+                {
+                    mr.enabled = visible;
+                }
+            }
+        }
+
+        if (MRUK.Instance != null && MRUK.Instance.GetCurrentRoom() != null)
+        {
+            GameObject roomObj = MRUK.Instance.GetCurrentRoom().gameObject;
+            MeshRenderer[] roomRenderers = roomObj.GetComponentsInChildren<MeshRenderer>(true);
+            foreach (MeshRenderer r in roomRenderers)
+            {
+                r.enabled = visible;
+            }
+        }
     }
 }
