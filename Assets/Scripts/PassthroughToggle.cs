a95bece05fb72f799b1b7a9e424d704f8c908f99
Pablo Gómez Rivas <pgr866@inlumine.ual.es>
2026-05-03 20:38:56 +0200

    mr totalmente personalizado y controlado. solo falta pintar en él

--- Assets/Scripts/PassthroughToggle.cs
+++ Assets/Scripts/PassthroughToggle.cs
@@ -8,76 +8,20 @@ public class PassthroughToggler : MonoBehaviour
     [SerializeField] private Camera mainCamera;
     [SerializeField] private GameObject locomotionSystem;
 
-    public bool IsMRActive { get; private set; } = false;
-
-    private void Awake()
-    {
-        UnityEngine.XR.XRSettings.useOcclusionMesh = false;
-    }
-
-    private void Start()
-    {
-        ToggleMR(false);
-    }
+    private void Start() => ToggleMR(false);
 
     public void ToggleMR(bool enableMR)
     {
-        IsMRActive = enableMR;
-
-        if (passthroughLayer != null) passthroughLayer.enabled = enableMR;
-
-        if (mainCamera != null)
+        passthroughLayer.enabled = enableMR;
+        locomotionSystem.SetActive(!enableMR);
+        MRUK.Instance.EnableWorldLock = enableMR;
+        mainCamera.clearFlags = enableMR ? CameraClearFlags.SolidColor : CameraClearFlags.Skybox;
+        if (enableMR) MRUK.Instance.LoadSceneFromDevice();
+        foreach (var em in Resources.FindObjectsOfTypeAll<EffectMesh>())
         {
-            if (enableMR)
-            {
-                mainCamera.clearFlags = CameraClearFlags.SolidColor;
-                mainCamera.backgroundColor = new Color(0, 0, 0, 0);
-            }
-            else
-            {
-                mainCamera.clearFlags = CameraClearFlags.Skybox;
-            }
+            if (em.gameObject.scene.name != null) em.gameObject.SetActive(enableMR);
         }
-
-        if (locomotionSystem != null) locomotionSystem.SetActive(!enableMR);
-
-        if (MRUK.Instance != null)
-        {
-            MRUK.Instance.EnableWorldLock = enableMR;
-            if (enableMR && MRUK.Instance.GetCurrentRoom() == null)
-            {
-                MRUK.Instance.LoadSceneFromDevice();
-            }
-        }
-
-        ToggleAllEffectMeshes(enableMR);
-        Debug.Log("Modo MR: " + (enableMR ? "ON (Bloqueado & Locomotion OFF)" : "OFF (Libre & Locomotion ON)"));
-    }
-
-    private void ToggleAllEffectMeshes(bool visible)
-    {
-        EffectMesh[] allMeshes = Resources.FindObjectsOfTypeAll<EffectMesh>();
-        foreach (EffectMesh em in allMeshes)
-        {
-            if (em.gameObject.scene.name != null)
-            {
-                em.enabled = visible;
-                MeshRenderer[] renderers = em.GetComponentsInChildren<MeshRenderer>(true);
-                foreach (MeshRenderer mr in renderers)
-                {
-                    mr.enabled = visible;
-                }
-            }
-        }
-
-        if (MRUK.Instance != null && MRUK.Instance.GetCurrentRoom() != null)
-        {
-            GameObject roomObj = MRUK.Instance.GetCurrentRoom().gameObject;
-            MeshRenderer[] roomRenderers = roomObj.GetComponentsInChildren<MeshRenderer>(true);
-            foreach (MeshRenderer r in roomRenderers)
-            {
-                r.enabled = visible;
-            }
-        }
+        if (MRUK.Instance?.GetCurrentRoom() != null) MRUK.Instance.GetCurrentRoom().gameObject.SetActive(enableMR);
+        Debug.Log("Modo MR: " + (enableMR ? "ON" : "OFF"));
     }
 }
